05e84d29109c8b8ec74205f7f25b04879b4d986c
Konrad Ciemała <konradciemala@Konrads-MacBook-Air.local>
2026-05-08 13:29:39 +0200

    project setup

--- App/main.py
+++ App/main.py
@@ -1,8 +1,9 @@
 import cv2
+import mediapipe as mp
 
 print("Uruchomienie programu")
-
 camera = cv2.VideoCapture(0)
+
 while True:
 
     success, frame = camera.read()
@@ -12,8 +13,9 @@ while True:
         break
 
     frame_flipped = cv2.flip(frame, 1)
+    image_rgb = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2RGB)
 
-    cv2.imshow("okienko", frame_flipped)
+    cv2.imshow("Sign Language", image_rgb)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Sign Language Recognition
 
 ---
+## Description
 
-### Description
-
-
----
+Desktop Application to recognise ASL sign language using AI.
 
 ---
+## Model Training
+- ### Dataset
+Dataset used in this project was taken from kaggle.com. Here is link for the dataset used: https://www.kaggle.com/search?q=ASL+Alphabet+in%3Adatasets
 
----
+- ### Algorithm
+
+
+
